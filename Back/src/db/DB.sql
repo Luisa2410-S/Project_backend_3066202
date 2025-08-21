48a83806ae5020c2ae07f85f8ca3449ff2e2a305
Avi004 <juansgamer1@gmail.com>
2025-08-21 11:08:40 -0500

    backend funcionando

--- Back/src/db/DB.sql
+++ Back/src/db/DB.sql
@@ -1,7 +1,20 @@
-CREATE DATABASE IF NOT EXISTS futbol_equipos;
+CREATE DATABASE futbol_equipos;
 USE futbol_equipos;
 
 CREATE TABLE equipos(
     id_equipo INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
+    nombre_equipo VARCHAR(100) NOT NULL
 );
+
+-- Insert = POST
+
+Insert Into equipos(nombre_equipo)
+Values
+("A"),
+("B"),
+("C"),
+("D"),
+("E");
+
+-- Select = GET
+Select * From equipos;
